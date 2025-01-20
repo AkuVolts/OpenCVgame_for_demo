dbeb9a53a7e1c7c876220e7ee96c0cdaeedc1788
AkuVolts <81880733+AkuVolts@users.noreply.github.com>
2025-01-19 23:21:41 -0600

    python turtle demo

---
+++ python-turtle-demo.py
@@ -0,0 +1,34 @@
+import math
+import turtle
+import time
+
+# Set up the screen
+turtle.setup(800, 600)
+turtle.title("Python Turtle Graphics Demo")
+
+# Set up the turtle
+turtle.shape("turtle")
+turtle.speed(0)
+turtle.bgcolor("white")
+turtle.color("black")
+turtle.pensize(1)
+
+# make the turtle
+turtle = turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)
+turtle.color("red")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+
+time.sleep(20)
+
+
+
+# do not close the window once drawing is over
